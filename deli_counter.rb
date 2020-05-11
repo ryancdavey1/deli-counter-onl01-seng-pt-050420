2445dd2f2011b5c0233971ec923a5072a9096adf
Ryan D <ryancdavey1@gmail.com>
2020-05-11 17:27:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
   else
       line_array = "The line is currently:"
       array.each_with_index do |name, index|
-        line_array << ()
+        line_array << ("#{index + 1}. #{name}")
       end
   end
   
